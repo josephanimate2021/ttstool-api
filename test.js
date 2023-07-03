352c048b71f7fb391d9d4a5c1ac61771c31b625e
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2023-07-03 19:59:51 -0400

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,10 @@ tts.getVoices().then(inf => { // get the voices
                 for (let i = 0; i < 10; i++) { // limit is set to 10 because ttstool cannot handle more than 10 requests.
                         tts.genMp3(keys[i], 'test').then(buffer => { // generate an mp3 buffer
                                 console.log(buffer);
-                                if (buffer) fs.writeFileSync(`./files/${keys[i]}.mp3`, buffer);
+                                if (buffer) {
+                                        if (!fs.existsSync('./files')) fs.mkdirSync('./files');
+                                        fs.writeFileSync(`./files/${keys[i]}.mp3`, buffer);
+                                }
                         }).catch(console.error);
                 }
         }
